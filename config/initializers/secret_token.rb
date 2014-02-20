8d738c3848062a8d6d18544a3d5510ebc13564d5
Randal Lilly <ran_lilly77@yahoo.com>
2014-02-20 11:13:20 +0000

    todo list

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoApp::Application.config.secret_key_base = '6291a32a0fa6f801f8aa5dda03a58d17d4b2a094525d2bc545c4c62a26cb1554379177db58fc6dab4ba764be04b8c055f0d4581670662a8f158f684e22161e06'
